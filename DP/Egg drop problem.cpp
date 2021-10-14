64f2aa7be87516468370d892c9bed293de89eafe
SUHAS M <suhas.m1972@gmail.com>
2021-10-14 23:45:04 +0530

    Added permutation program using C++

--- DP/Egg drop problem.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-  int a, b=10, c=10;
-  a=(b==c);
-  cout<<a;
-  return 0;
-}
--- Greedy/Untitled1.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	int num=4;
-	num=45>23?:3;
-	cout<<num;
-}
